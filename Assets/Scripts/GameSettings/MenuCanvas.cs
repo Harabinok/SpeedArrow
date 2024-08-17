57d4bbad77c0193dd6192fb8a68a3b0cabf80bfa
Harabinok <87910293+Harabinok@users.noreply.github.com>
2024-08-17 20:43:39 +0600

    Добавил меню

---
+++ Assets/Scripts/GameSettings/MenuCanvas.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuCanvas : MonoBehaviour
+{
+    [SerializeField] private int sceanIndex;
+    [SerializeField] private GameObject[] Levels;
+
+    [Header("UI")]
+    [SerializeField] private Button start;
+    [SerializeField] private Button next;
+    [SerializeField] private Button back;
+    private void Awake()
+    {
+        for (int i = 0; i < Levels.Length; i++)
+        {
+            if (Levels[i].gameObject.activeSelf == true) sceanIndex = i;
+        }
+    }
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(sceanIndex);
+    }
+    public void NextLevel()
+    {
+        sceanIndex++;
+        if (sceanIndex >= Levels.Length)
+        {
+            sceanIndex = Levels.Length;
+            next.interactable = false;
+            return;
+        }
+        else next.interactable = true;
+        for (int i = 0; i < Levels.Length; i++)
+        {
+            Levels[i].SetActive(false);
+        }
+        Levels[sceanIndex].SetActive(true);
+        back.interactable = true;
+    }
+    public void BackLevel()
+    {
+        sceanIndex--;
+        if (sceanIndex < 0)
+        {
+            sceanIndex = 0;
+            back.interactable = false;
+            return;
+        }
+        else back.interactable = true;
+        for (int i = 0; i < Levels.Length; i++)
+        {
+            Levels[i].SetActive(false);
+        }
+        Levels[sceanIndex].SetActive(true);
+        next.interactable = true;
+    }
+}
